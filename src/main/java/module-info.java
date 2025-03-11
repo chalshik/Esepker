558e65bf57ce030216488ebb29f684c90e3f6002
chalshik <nurmuhammedkalmamatov06@gmail.com>
2025-03-11 14:32:57 +0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.esepkersoft {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.esepkersoft to javafx.fxml;
+    exports com.example.esepkersoft;
+}
