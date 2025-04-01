9c9fa1b9b5382b4c0034b60f3e851ee72fd1571b
chalshik <nurmuhammedkalmamatov06@gmail.com>
2025-04-01 12:58:21 +0600

    to test

---
+++ src/main/java/com/example/esepkersoft/Models/CountProducts.java
@@ -0,0 +1,38 @@
+package com.example.esepkersoft.Models;
+
+
+public class CountProducts{
+    private String barcode;
+    private String name;
+    private double purchase_price;
+    private double sale_price;
+    private double quantity;
+
+    public CountProducts(String barcode, String name, double purchase_price, double sale_price, double quantity) {
+        this.barcode = barcode;
+        this.name = name;
+        this.purchase_price = purchase_price;
+        this.sale_price = sale_price;
+        this.quantity = quantity;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPurchase_price() {
+        return purchase_price;
+    }
+
+    public double getSale_price() {
+        return sale_price;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+}
